a0462163680be2e4c42bfe813c97aeb0643190d2
Ilya Novak <rickmortyand4@gmail.com>
2022-03-19 11:11:09 +0300

    test

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    const x = 10;
+    return this.appService.getHello();
+  }
+}
