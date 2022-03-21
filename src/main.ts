a3e7a07d5d08fbf2b9c1812f6d409f4e70754c8e
Ilya Novak <ilya.novak.dev@gmail.com>
2022-03-21 16:48:51 +0300

    new commif1

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('Ba');
+  console.log('Bad!');
   await app.listen(3000);
 }
 bootstrap();
